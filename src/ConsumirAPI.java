5f891918e0f9a57d3bd55cdf380a634aa6e13c51
OsvaldoCorneli <overcorneli@gmail.com>
2024-10-10 19:11:19 -0300

    cree consumir api y mensaje en el principal

--- src/ConsumirAPI.java
+++ src/ConsumirAPI.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class ConsumirAPI {
+import com.google.gson.Gson;
+import java.util.Map;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumirAPI {
+
+    Monedas encontrarValor(String moneda) throws IOException, InterruptedException {
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/fc33ae91f7a56e3bc0f73fd0/latest/USD");
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        Monedas respuesta = new Gson().fromJson(response.body(), Monedas.class);
+
+        Map<String, Double> conversionRates = respuesta.conversion_rates();
+        Double valor = conversionRates.get(moneda);
+
+        if (valor != null) {
+            System.out.println("El valor de " + moneda + " es: " + valor);
+        } else {
+            System.out.println("La clave " + moneda + " no se encuentra en el mapa.");
+        }
+
+
+        System.out.println("monedas " + respuesta.conversion_rates());
+
+        return new Gson().fromJson(response.body(), Monedas.class);
+    }
+
+
+
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Principal {
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        ConsumirAPI consulta = new ConsumirAPI();
+
+        String mensaje = """
+                **************************************************
+                
+                ¡Bienvenida/o al conversor de moneda de ONE!
+                
+                
+                1) Dolar =>> Peso Argentino.
+                2) Peso Argentino =>> Dolar.
+                3) Dolar =>> Real Brasileño
+                4) Real Brasileño =>> Dolar.
+                5) Dolar =>> Peso Colombiano
+                6) Peso Colombiano =>> Dolar
+                9) ¡Salir!
+                
+                **************************************************
+                
+                Por favor ingrese una opcion valida:
+                """;
+
+        while(true){
+
+            System.out.println(mensaje);
+            Scanner teclado = new Scanner(System.in);
+
+            int opcion = teclado.nextInt();
+            if(opcion == 9){
+                System.out.println("¡Gracias por utilizar nuestro conversor!");
+                break;
+            }
+            System.out.println(opcion);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+       // Monedas respuesta = consulta.encontrarValor("ARS");
+
+      //  System.out.println(respuesta);
+
+    }
 }
